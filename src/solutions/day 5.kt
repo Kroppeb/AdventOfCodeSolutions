79e91dc2ece34bdbaae67bbf86fcabb38cbc0311
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2019-12-06 05:49:20 +0100

    preparing for day 6
    Signed-off-by: Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>

--- src/solutions/day 5.kt
+++ src/solutions/day 5.kt
@@ -4,13 +4,18 @@ import helpers.*
 import kotlin.math.*
 
 private fun part1(data:List<Int>){
+	IntComputer(data,listOf(1)).run()
+}
+
+private fun part2(data:List<Int>){
 	IntComputer(data,listOf(5)).run()
 }
 
-
 fun main(){
 	val data = getInts(5)
+	IntComputer(data).run()
 	part1(data)
+	part2(data)
 
 
 
---
+++ src/solutions/day 6.kt
@@ -0,0 +1,32 @@
+package solutions
+
+import helpers.*
+import kotlin.math.*
+
+fun part1(data:List<>){
+
+}
+
+fun main(){
+	val data = get(6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
