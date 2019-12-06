bcc7c47cb6a92f14c9445c3757bd37ea1ed70d82
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2019-12-06 06:26:04 +0100

    day 6
    Signed-off-by: Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>

--- src/solutions/day 6.kt
+++ src/solutions/day 6.kt
@@ -3,13 +3,44 @@ package solutions
 import helpers.*
 import kotlin.math.*
 
-fun part1(data:List<>){
+fun part1(data:List<List<String>>){
+	val parent = data.map { (a,b) -> b to a}.toMap()
+	val leaves = parent.keys.toMutableSet().also{it.removeAll(parent.values)}
+
+	var orbits = parent.keys.toList()
+	var count = 0
+	while(orbits.isNotEmpty()){
+		count += orbits.size
+		orbits = orbits.mapNotNull{parent[it]}.filter{it in parent}
+	}
+	println(count)
+}
+
+fun part2(data:List<List<String>>){
+	val parent = data.map { (a,b) -> b to a}.toMap()
+	val leaves = parent.keys.toMutableSet().also{it.removeAll(parent.values)}
+
+	fun steps(location: MutableMap<String,Int>, steps:Int, self:String){
+		location[self] = steps
+		parent[self]?.let{steps(location, steps +1,it)}
+	}
+
+	val stepsFromMe = mutableMapOf<String,Int>()
+	steps(stepsFromMe, -1, "YOU")
+	fun stepsS(steps:Int, self:String){
+		stepsFromMe[self]?.let{error(it + steps)}
+		parent[self]?.let{stepsS(steps +1,it)}
+	}
+	stepsS(-1,"SAN")
+
+
 
 }
 
 fun main(){
-	val data = get(6)
-
+	val data = getLines(6).map{it.split(")")}
+	part1(data)
+	part2(data)
 
 
 
