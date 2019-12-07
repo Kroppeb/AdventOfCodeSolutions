f89a240ce1619f84f7591327df62e3429486935d
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2019-12-07 05:59:04 +0100

    pre 6
    Signed-off-by: Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>

---
+++ src/solutions/day 7.kt
@@ -0,0 +1,20 @@
+package solutions
+
+import helpers.*
+import kotlin.math.*
+
+private fun part1(data:List<String>){
+}
+
+private fun part2(data:List<String>){
+}
+
+fun main(){
+	val test = getLines(1019_06)
+	part1(test)
+	part2(test)
+
+	val data = getLines(6)
+	part1(data)
+	part2(data)
+}
