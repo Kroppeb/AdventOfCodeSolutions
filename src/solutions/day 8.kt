fb5cfa921c5c93fb0dbfb3ad1c7da87694fff65a
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2019-12-08 05:55:42 +0100

    Pre day 8
    Signed-off-by: Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>

---
+++ src/solutions/day 8.kt
@@ -0,0 +1,25 @@
+package solutions
+
+import helpers.*
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.TimeUnit
+import kotlin.math.*
+
+private fun part1(data: List<Int>) {
+
+}
+
+private fun part2(data: List<Int>) {
+
+}
+
+fun main() {
+
+	val test = get(1019_08)
+	part1(test)
+	part2(test)
+	val data = get(8)
+	part1(data)
+	part2(data)
+}
