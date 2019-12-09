9ebd031b4fd1d8d45c7a0b776e0dcc6f6c0f6d7b
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2019-12-09 12:59:43 +0100

    salty day 9
    Signed-off-by: Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>

--- src/helpers/IntComputer.kt
+++ src/helpers/IntComputer.kt
@@ -1,20 +1,22 @@
 package helpers
 
-class IntComputer(val data: MutableList<Int>, val inputs: Iterator<Int>) {
-	var ip = 0
+import java.math.BigInteger
+
+class BigIntegerComputer(val data: MutableMap<BigInteger, BigInteger>, val inputs: Iterator<BigInteger>) {
+	var ip = BigInteger.ZERO
 	var finished = false
 
 
-	fun run(): Int {
+	fun run(): BigInteger {
 		y = false
 		while (!finished && !y) {
 			step()
 		}
-		return this[0]
+		return this[BigInteger.ZERO]
 	}
 
 	fun step() {
-		val i = this[ip]
+		val i = this[ip].toInt()
 		val opd = i % 100
 		val op = ops[opd]!!
 		// original
@@ -22,43 +24,51 @@ class IntComputer(val data: MutableList<Int>, val inputs: Iterator<Int>) {
 
 		/*
 		suggestions
-		val modes = (i / 100).toString().map{"$it".toInt()} + listOf(0,0,0,0,0,0,0)
+		val modes = (i / 100).toString().map{"$it".toBigInteger()} + listOf(0,0,0,0,0,0,0)
 		or
-		val modes = (i / 100).toString().map{"$it".toInt()}
+		val modes = (i / 100).toString().map{"$it".toBigInteger()}
 		*/
 
-		val modes = generateStateTimes(op.size, i / 100) {
+		val modes = generateStateTimes(op.size, i.toInt() / 100) {
 			it / 10 to it % 10
 		}
 		execute(op, modes)
 	}
 
-	operator fun get(inp: Int): Int {
-		return data[inp]
+	operator fun get(inp: BigInteger): BigInteger {
+		return data[inp]!!
 	}
 
-	operator fun get(inp: Param): Int = when (inp.first) {
-		0 -> data[inp.second]
+	operator fun get(inp: Param): BigInteger = when (inp.first) {
+		0 -> data.getOrDefault(inp.second, BigInteger.ZERO) // enen though I made this a default dict ?
 		1 -> inp.second
+		2 -> data.getOrDefault(inp.second + rb, BigInteger.ZERO)
 		else -> error("n")
 	}
 
-	operator fun set(inp: Int, value: Int) {
+	operator fun set(inp: BigInteger, value: BigInteger) {
 		data[inp] = value
 	}
 
-	operator fun set(inp: Param, value: Int) {
-		data[inp.second] = value
+	operator fun set(inp: Param, value: BigInteger) {
+		data[+inp] = value
 	}
 
-	operator fun Param.not() = this@IntComputer[this]
+	operator fun Param.not() = this@BigIntegerComputer[this]
 
+	operator fun Param.unaryPlus() = when(first){
+		0 -> second
+		2 -> second+rb
+		else -> error("Fuck you")
+	}
 
 	fun execute(op: Op, modes: List<Int>) {
 		val oldIp = ip
-		ip += op.size
-		op.action(this, modes.zip(data.subList(oldIp + 1, oldIp + op.size)))
-		if(y){
+		ip += op.size.toBigInteger()
+		op.action(this, modes.zip((1 until op.size).map {
+			data[oldIp + it.toBigInteger()]!!
+		}))
+		if (y) {
 			ip = oldIp
 		}
 	}
@@ -69,34 +79,36 @@ class IntComputer(val data: MutableList<Int>, val inputs: Iterator<Int>) {
 
 	var y = false
 
-	fun yield(){
+	fun yield() {
 		y = true
 	}
 
-	var last:Int = 0
+	var last: BigInteger = BigInteger.ZERO
+
+	var rb: BigInteger = BigInteger.ZERO
 
 	companion object {
-		operator fun invoke(data: List<Int>, inputs: Iterable<Int>? = null) =
-				IntComputer(
-						data.toMutableList(),
+		operator fun invoke(data: List<BigInteger>, inputs: Iterable<BigInteger>? = null) =
+				BigIntegerComputer(
+						data.withIndex().associate { (i, v)-> i.toBigInteger() to v }.toMutableMap().withDefault { BigInteger.ZERO },
 						inputs?.iterator()
-								?: System.`in`.bufferedReader().lineSequence().map { it.toInt() }.iterator()
+								?: System.`in`.bufferedReader().lineSequence().map { it.toBigInteger() }.iterator()
 				)
 
 		val ops: Map<Int, Op> = opBuilder {
 			1{ a, b, c -> this[c] = !a + !b }
 			2{ a, b, c -> this[c] = !a * !b }
-			3{ a -> inputs.getNext().let{
-				if(it == -1337)
-					yield()
-				else
-					this[a.second] = it
-			} }
+			3{ a ->
+				inputs.getNext().let {
+					this[a] = it
+				}
+			}
 			4{ a -> last = !a;println(!a) }
-			5{ a, b -> if (!a != 0) ip = !b }
-			6{ a, b -> if (!a == 0) ip = !b }
-			7{ a, b, c -> this[c] = if (!a < !b) 1 else 0 }
-			8{ a, b, c -> this[c] = if (!a == !b) 1 else 0 }
+			5{ a, b -> if (!a != BigInteger.ZERO) ip = !b }
+			6{ a, b -> if (!a == BigInteger.ZERO) ip = !b }
+			7{ a, b, c -> this[c] = if (!a < !b) BigInteger.ONE else BigInteger.ZERO }
+			8{ a, b, c -> this[c] = if (!a == !b) BigInteger.ONE else BigInteger.ZERO }
+			9{ a -> rb += !a }
 			99{ -> halt() }
 		}
 
@@ -109,39 +121,39 @@ class IntComputer(val data: MutableList<Int>, val inputs: Iterator<Int>) {
 		class OpBuilder {
 			val ops = mutableMapOf<Int, Op>()
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.() -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.() -> Unit) {
 				ops[this] = Op(1) { preOp() }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param) -> Unit) {
 				ops[this] = Op(2) { (a) -> preOp(a) }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param, Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param, Param) -> Unit) {
 				ops[this] = Op(3) { (a, b) -> preOp(a, b) }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param, Param, Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param, Param, Param) -> Unit) {
 				ops[this] = Op(4) { (a, b, c) -> preOp(a, b, c) }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param, Param, Param, Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param, Param, Param, Param) -> Unit) {
 				ops[this] = Op(5) { (a, b, c, d) -> preOp(a, b, c, d) }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param, Param, Param, Param, Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param, Param, Param, Param, Param) -> Unit) {
 				ops[this] = Op(6) { (a, b, c, d, e) -> preOp(a, b, c, d, e) }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param, Param, Param, Param, Param, Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param, Param, Param, Param, Param, Param) -> Unit) {
 				ops[this] = Op(7) { (a, b, c, d, e, f) -> preOp(a, b, c, d, e, f) }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param, Param, Param, Param, Param, Param, Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param, Param, Param, Param, Param, Param, Param) -> Unit) {
 				ops[this] = Op(8) { (a, b, c, d, e, f, g) -> preOp(a, b, c, d, e, f, g) }
 			}
 
-			inline operator fun Int.invoke(crossinline preOp: IntComputer.(Param, Param, Param, Param, Param, Param, Param, Param) -> Unit) {
+			inline operator fun Int.invoke(crossinline preOp: BigIntegerComputer.(Param, Param, Param, Param, Param, Param, Param, Param) -> Unit) {
 				ops[this] = Op(9) { (a, b, c, d, e, f, g, h) -> preOp(a, b, c, d, e, f, g, h) }
 			}
 
@@ -149,9 +161,9 @@ class IntComputer(val data: MutableList<Int>, val inputs: Iterator<Int>) {
 	}
 }
 
-typealias Param = Pair<Int, Int>
+typealias Param = Pair<Int, BigInteger>
 
-class Op(val size: Int, val action: IntComputer.(List<Param>) -> Unit)
+class Op(val size: Int, val action: BigIntegerComputer.(List<Param>) -> Unit)
 
 
 
