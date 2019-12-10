21f4cc1bcebacf06f93b1edf92b49e0d1d7be43d
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2019-12-10 12:11:17 +0100

    finished IntComputer for now
    Signed-off-by: Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>

---
+++ src/coroutines/iterable.kt
@@ -0,0 +1,9 @@
+package coroutines
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
+suspend inline fun <T,R>Iterable<T>.parallelMap(crossinline transform:suspend (T)->R) = coroutineScope{
+	this@parallelMap.map{async { transform(it) }}.awaitAll()
+}
