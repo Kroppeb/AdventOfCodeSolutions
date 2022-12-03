649196286a6825d4c1673d76ee8cc8ccead0207d
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2022-12-03 06:09:20 +0100

    2022 Day 3
    Signed-off-by: Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>

--- solutions/src/solutions/y22/day 3.kt
+++ solutions/src/solutions/y22/day 3.kt
@@ -1,6 +1,6 @@
 @file:Suppress("PackageDirectoryMismatch", "UnusedImport")
 
-package solutions.y22
+package solutions.y22.d03
 
 
 /*
@@ -27,8 +27,16 @@ import kotlin.math.*
 private val xxxxx = Clock(6, 3);
 
 private fun part1() {
-	var data = getLines(3).e().map{it.chunked(it.size / 2)}.sumOf{(a,b) ->
+	var data = getLines(2022_03).e().map{it.chunked(it.size / 2)}.sumOf{(a,b) ->
 		val p = a.toSet().intersect(b.toSet()).iterator().next()
+		if (p in 'a'..'z') p - 'a' + 1 else p - 'A' + 27
+	}.log()
+}
+
+
+private fun part2() {
+	var data = getLines(2022_03).e().chunked(3).sumOf{(a,b,c ) ->
+		val p = a.toSet().intersect(b.toSet()).intersect(c.toSet()).iterator().next()
 		if (p in 'a'..'z') p - 'a' + 1 else p - 'A' + 27
 	}.log()
 }
@@ -37,6 +45,7 @@ private fun part1() {
 fun main() {
 	println("Day 3: ")
 	part1()
+	part2()
 }
 
 
